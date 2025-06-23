bede737a6c1ae2ec4291a93cd57559054da69602
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2025-06-23 10:54:27 +0200

    Add plain text data presentation

--- src/controllers/TimeSeriesController.tsx
+++ src/controllers/TimeSeriesController.tsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react";
 
-const API_BASE_URL = 'https://energy-charts.free.beeceptor.com'
+// const API_BASE_URL = 'https://energy-charts.free.beeceptor.com'
 
 interface telemetryDataset {
     timestamp: string;
@@ -12,13 +12,17 @@ export default function TimeSeriesController() {
     const [error, setError] = useState();
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [datasets, setDataset] = useState<telemetryDataset[]>([]);
+    const[page, setPage] = useState(0);
+
+    const apiUrl = process.env.REACT_APP_API_BASE_URL;
+
 
     useEffect(() => {
         const fetchTelemetry = async () => {
             setIsLoading(true);
             try {
 
-                const response = await fetch(`${API_BASE_URL}/timeSeries`);
+                const response = await fetch(`${apiUrl}/timeSeries`);
                 const datasets = await response.json() as telemetryDataset[];
                 setDataset(datasets);
             } catch (error: any) {
@@ -29,23 +33,28 @@ export default function TimeSeriesController() {
             }
         };
         fetchTelemetry();
-    }, [])
+    }, [page])
 
     if (isLoading) {
         return <div>Loading...</div>;
     }
 
     if (error) {
-        return <div>Something went wrong, Please try again later</div>
+        return <div>Something went wrong. Please try again later</div>
     }
+
+    const indexedDatasets = datasets.map((dataset, index) =>({
+        ...dataset, index: index + 1
+    }));
     return (
         <div className='container'>
             <h1 className='title'>Time Series</h1>
-            <ul>
-                {datasets.map((dataset) => {
-                    return <li key={dataset.timestamp}>{dataset.consumption}, {dataset.price}</li>
+            <ul style={{listStyleType: 'none'}}>
+                {indexedDatasets.map((dataset, index) => {
+                    return <li
+                        key={dataset.timestamp}> #{dataset.index} - {dataset.consumption} kWh, {dataset.price} PLN/kWh</li>
                 })
-                })
+                }
             </ul>
         </div>
     )
